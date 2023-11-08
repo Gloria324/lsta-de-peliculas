31b167e7c49202a7b9b7257f8bcd2702f3b0aa2c
glori <glori@GLORIA>
2023-11-08 04:21:28 -0300

    Agregar archivos de proyecto.

---
+++ lsta de peliculas/Form1.Designer.cs
@@ -0,0 +1,265 @@
+﻿namespace lsta_de_peliculas
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+    
+
+       
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btnAgregar = new System.Windows.Forms.Button();
+            this.dataGridPelicula = new System.Windows.Forms.DataGridView();
+            this.Titulo = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Director = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Año = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Genero = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Pelicula = new System.Windows.Forms.Label();
+            this.btnModificar = new System.Windows.Forms.Button();
+            this.btnEliminar = new System.Windows.Forms.Button();
+            this.TituloTextBox = new System.Windows.Forms.TextBox();
+            this.añoTextBox = new System.Windows.Forms.TextBox();
+            this.directorTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.generoTextBox = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnSalir = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridPelicula)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+         
+            this.btnAgregar.ForeColor = System.Drawing.SystemColors.Desktop;
+            this.btnAgregar.Location = new System.Drawing.Point(74, 238);
+            this.btnAgregar.Name = "btnAgregar";
+            this.btnAgregar.Size = new System.Drawing.Size(150, 47);
+            this.btnAgregar.TabIndex = 4;
+            this.btnAgregar.Text = "AGREGAR PELICULA";
+            this.btnAgregar.UseVisualStyleBackColor = true;
+            this.btnAgregar.Click += new System.EventHandler(this.btnAgregar_Click);
+         
+            
+            this.dataGridPelicula.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridPelicula.BackgroundColor = System.Drawing.SystemColors.HighlightText;
+            this.dataGridPelicula.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridPelicula.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Titulo,
+            this.Director,
+            this.Año,
+            this.Genero});
+            this.dataGridPelicula.Location = new System.Drawing.Point(325, 382);
+            this.dataGridPelicula.Name = "dataGridPelicula";
+            this.dataGridPelicula.RowHeadersWidth = 51;
+            this.dataGridPelicula.RowTemplate.Height = 24;
+            this.dataGridPelicula.Size = new System.Drawing.Size(883, 252);
+            this.dataGridPelicula.TabIndex = 6;
+         
+            
+            this.Titulo.HeaderText = "Titulo";
+            this.Titulo.MinimumWidth = 6;
+            this.Titulo.Name = "Titulo";
+          
+            this.Director.HeaderText = "Director";
+            this.Director.MinimumWidth = 6;
+            this.Director.Name = "Director";
+            
+            
+            this.Año.HeaderText = "Año";
+            this.Año.MinimumWidth = 6;
+            this.Año.Name = "Año";
+           
+            this.Genero.HeaderText = "Genero";
+            this.Genero.MinimumWidth = 6;
+            this.Genero.Name = "Genero";
+            
+            
+            this.Pelicula.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Pelicula.ForeColor = System.Drawing.SystemColors.Desktop;
+            this.Pelicula.Location = new System.Drawing.Point(319, 318);
+            this.Pelicula.Name = "Pelicula";
+            this.Pelicula.Size = new System.Drawing.Size(238, 49);
+            this.Pelicula.TabIndex = 9;
+            this.Pelicula.Text = "PELICULAS";
+            this.Pelicula.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+           
+            this.btnModificar.ForeColor = System.Drawing.SystemColors.Desktop;
+            this.btnModificar.Location = new System.Drawing.Point(74, 300);
+            this.btnModificar.Name = "btnModificar";
+            this.btnModificar.Size = new System.Drawing.Size(150, 47);
+            this.btnModificar.TabIndex = 10;
+            this.btnModificar.Text = "MODIFICAR\r\n\r\n\r\n\r\n\r\n\r\n\r\n";
+            this.btnModificar.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.btnModificar.UseVisualStyleBackColor = true;
+            this.btnModificar.Click += new System.EventHandler(this.btnModificar_Click);
+          
+            this.btnEliminar.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.btnEliminar.Location = new System.Drawing.Point(74, 366);
+            this.btnEliminar.Name = "btnEliminar";
+            this.btnEliminar.Size = new System.Drawing.Size(150, 47);
+            this.btnEliminar.TabIndex = 11;
+            this.btnEliminar.Text = "ELIMINAR";
+            this.btnEliminar.UseVisualStyleBackColor = true;
+            this.btnEliminar.Click += new System.EventHandler(this.btnEliminar_Click);
+            
+            this.TituloTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TituloTextBox.Location = new System.Drawing.Point(325, 66);
+            this.TituloTextBox.Name = "TituloTextBox";
+            this.TituloTextBox.Size = new System.Drawing.Size(883, 30);
+            this.TituloTextBox.TabIndex = 13;
+           
+            this.añoTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.añoTextBox.Location = new System.Drawing.Point(325, 238);
+            this.añoTextBox.Name = "añoTextBox";
+            this.añoTextBox.Size = new System.Drawing.Size(377, 30);
+            this.añoTextBox.TabIndex = 14;
+            
+
+            this.directorTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.directorTextBox.Location = new System.Drawing.Point(325, 150);
+            this.directorTextBox.Name = "directorTextBox";
+            this.directorTextBox.Size = new System.Drawing.Size(883, 30);
+            this.directorTextBox.TabIndex = 15;
+           
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.Desktop;
+            this.label1.Location = new System.Drawing.Point(320, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(80, 29);
+            this.label1.TabIndex = 16;
+            this.label1.Text = "Titulo";
+            
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.Desktop;
+            this.label2.Location = new System.Drawing.Point(320, 195);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(249, 29);
+            this.label2.TabIndex = 17;
+            this.label2.Text = "Año de Lanzamiento";
+            
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.SystemColors.Desktop;
+            this.label3.Location = new System.Drawing.Point(320, 109);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(106, 29);
+            this.label3.TabIndex = 18;
+            this.label3.Text = "Director";
+           
+            this.generoTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.generoTextBox.Location = new System.Drawing.Point(767, 238);
+            this.generoTextBox.Name = "generoTextBox";
+            this.generoTextBox.Size = new System.Drawing.Size(441, 30);
+            this.generoTextBox.TabIndex = 19;
+            
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.SystemColors.Desktop;
+            this.label4.Location = new System.Drawing.Point(762, 199);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(100, 29);
+            this.label4.TabIndex = 20;
+            this.label4.Text = "Genero";
+           
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(281, 168);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 21;
+            this.pictureBox1.TabStop = false;
+           
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(1081, 392);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(8, 8);
+            this.dataGridView1.TabIndex = 22;
+          
+            this.btnSalir.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.btnSalir.Location = new System.Drawing.Point(74, 433);
+            this.btnSalir.Name = "btnSalir";
+            this.btnSalir.Size = new System.Drawing.Size(150, 47);
+            this.btnSalir.TabIndex = 23;
+            this.btnSalir.Text = "SALIR\r\n";
+            this.btnSalir.UseVisualStyleBackColor = true;
+            this.btnSalir.Click += new System.EventHandler(this.btnSalir_Click);
+          
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.ClientSize = new System.Drawing.Size(1260, 686);
+            this.ControlBox = false;
+            this.Controls.Add(this.btnSalir);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.generoTextBox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.directorTextBox);
+            this.Controls.Add(this.añoTextBox);
+            this.Controls.Add(this.TituloTextBox);
+            this.Controls.Add(this.btnEliminar);
+            this.Controls.Add(this.btnModificar);
+            this.Controls.Add(this.Pelicula);
+            this.Controls.Add(this.dataGridPelicula);
+            this.Controls.Add(this.btnAgregar);
+            this.ForeColor = System.Drawing.SystemColors.HighlightText;
+            this.HelpButton = true;
+            this.ImeMode = System.Windows.Forms.ImeMode.Disable;
+            this.KeyPreview = true;
+            this.Name = "Form1";
+            this.Text = "Mi Lista De Peliculas";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridPelicula)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+       
+        private System.Windows.Forms.Button btnAgregar;
+        private System.Windows.Forms.DataGridView dataGridPelicula;
+        private System.Windows.Forms.Label Pelicula;
+        private System.Windows.Forms.Button btnModificar;
+        private System.Windows.Forms.Button btnEliminar;
+        private System.Windows.Forms.TextBox TituloTextBox;
+        private System.Windows.Forms.TextBox añoTextBox;
+        private System.Windows.Forms.TextBox directorTextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox generoTextBox;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Titulo;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Director;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Año;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Genero;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnSalir;
+    }
+}
+
---
+++ lsta de peliculas/Form1.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.TaskbarClock;
+
+namespace lsta_de_peliculas
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnAgregar_Click(object sender, EventArgs e)
+        {
+           
+            string titulo = TituloTextBox.Text;
+            string director = directorTextBox.Text;
+            string genero = generoTextBox.Text;
+
+            if (int.TryParse(añoTextBox.Text, out int anio))
+            {
+                
+                dataGridPelicula.Rows.Add(titulo, director, anio, genero);
+
+                
+                TituloTextBox.Clear();
+                directorTextBox.Clear();
+                añoTextBox.Clear();
+                generoTextBox.Clear();
+            }
+            else
+            {
+                MessageBox.Show("El año debe ser un valor numérico válido.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+
+
+
+
+
+        private void btnSalir_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void btnModificar_Click(object sender, EventArgs e)
+        {
+            if (dataGridPelicula.SelectedRows.Count > 0)
+    {
+        
+        DataGridViewRow filaSeleccionada = dataGridPelicula.SelectedRows[0];
+
+       
+        string titulo = filaSeleccionada.Cells["Titulo"].Value.ToString();
+        string director = filaSeleccionada.Cells["Director"].Value.ToString();
+        int anio = Convert.ToInt32(filaSeleccionada.Cells["Año"].Value);
+        string genero = filaSeleccionada.Cells["Género"].Value.ToString();
+
+        
+        TituloTextBox.Text = titulo;
+        directorTextBox.Text = director;
+        añoTextBox.Text = anio.ToString();
+        generoTextBox.Text = genero;
+
+       
+        dataGridPelicula.Rows.Remove(filaSeleccionada);
+    }
+}
+
+        private void btnEliminar_Click(object sender, EventArgs e)
+        {
+            if (dataGridPelicula.SelectedRows.Count > 0) ;
+        }
+    } 
+ 
+}
+    
+    
